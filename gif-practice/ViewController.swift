9a40a1b83c19b99f98daaa8431f5111f16d710a0
ntouee6228 <ntouee6228@ntouee6228de-MacBook-Air.local>
2018-11-06 14:21:31 +0800

    Initial Commit

---
+++ gif-practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gif-practice
+//
+//  Created by ntouee6228 on 2018/11/6.
+//  Copyright © 2018 ntouee6228. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
